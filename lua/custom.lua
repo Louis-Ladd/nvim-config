7b75026c918256c7d16a58960a64a0184ea8921a
Louis-Ladd <lewisharshman1@gmail.com>
2025-01-18 12:45:14 -0700

    add css

--- lua/custom.lua
+++ lua/custom.lua
@@ -3,12 +3,12 @@ local M = {}
 
 -- add servers to be used for auto formatting here
 M.formatting_servers = {
-    rust_analyzer = {},
-    lua_ls = {},
-    clangd = {},
+  rust_analyzer = {},
+  lua_ls = {},
+  clangd = {},
 }
 
 -- add Tree-sitter to auto-install
-M.ensure_installed = { "c" }
+M.ensure_installed = { "c", "css" }
 
 return M
