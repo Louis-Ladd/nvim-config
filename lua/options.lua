f9fc4d60d65320bc37189b2d02ccb64efc53ccde
Louis-Ladd <lewisharshman1@gmail.com>
2025-08-19 01:05:25 -0600

    Switch over to tabs over spaces

--- lua/options.lua
+++ lua/options.lua
@@ -34,13 +34,9 @@ opt.matchpairs = { "(:)", "{:}", "[:]", "<:>" }
 opt.syntax = "enable"
 
 -- indention
-opt.autoindent = true    -- auto indentation
-opt.expandtab = false    -- convert tabs to spaces
-opt.shiftwidth = indent  -- the number of spaces inserted for each indentation
-opt.smartindent = true   -- make indenting smarter
-opt.softtabstop = indent -- when hitting <BS>, pretend like a tab is removed, even if spaces
-opt.tabstop = indent     -- insert 2 spaces for a tab
-opt.shiftround = true    -- use multiple of shiftwidth when indenting with "<" and ">"
+opt.autoindent = true  -- auto indentation
+opt.expandtab = false  -- convert tabs to spaces
+opt.smartindent = true -- make indenting smarter
 
 -- search
 opt.hlsearch = true   -- highlight all matches on previous search pattern
